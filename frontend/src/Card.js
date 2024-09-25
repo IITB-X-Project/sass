f881667f8406860e007f4bd1e44e43c973ba979d
[Puneet] <puneetchandna21@gmail.com>
2024-09-25 21:29:53 +0530

    updated

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -5,6 +5,7 @@ const ProductCard = ({ product }) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-4 flex flex-col justify-between h-full">
       <img
+        loading='lazy'
         src={product.image}
         alt={product.name}
         className="w-full h-48 object-cover rounded-md mb-4"
--- retailer/src/ShoppingCart/Product.js
+++ retailer/src/ShoppingCart/Product.js
@@ -4,6 +4,7 @@ export default function Product({ product, id, onQuantityChange, handleRemove })
       <div className="d-flex flex-wrap gap-3 align-items-center justify-content-center justify-content-lg-start">
         <div className="d-flex flex-column align-items-center">
           <img
+            loading="lazy"
             src={product.img}
             alt={product.name}
             className="img-fluid"

6bfa727fce365c703f285f7d810ab0d4b8fe4e16
Mohit Sharma <mohits9168@gmail.com>
2024-10-01 00:30:52 +0530

    UI Changes

--- Client/src/pages/CheckOutPage/Header.jsx
+++ Client/src/pages/CheckOutPage/Header.jsx
@@ -1,10 +1,24 @@
 import './Checkout.css';
 
-export default function Header() {
+function Header() {
+
     return (
-        <div className="header-container d-flex justify-content-around">
-            <img style={{maxWidth: '6em', maxHeight: '6em'}} className="logo" src="https://upload.wikimedia.org/wikipedia/en/1/1d/Indian_Institute_of_Technology_Bombay_Logo.svg" alt="logo" />
-            <h1 style={{fontSize: '3rem'}}>Checkout</h1>
-        </div>
+        <header className="bg-gray-800 shadow-md p-4 header">
+            <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+                <div className="logo text-slate-200 font-bold text-xl">
+                    <img
+                        width="65"
+                        height="60"
+                        src="https://upload.wikimedia.org/wikipedia/en/1/1d/Indian_Institute_of_Technology_Bombay_Logo.svg"
+                        alt="eCommerce IIT Bombay"
+                    />
+                </div>
+                <div className="flex space-x-4 mt-2 sm:mt-0">
+                    <h1 style={{fontSize: '3rem'}}>Checkout</h1>
+                </div>
+            </div>
+        </header>
     );
 }
+
+export default Header;
